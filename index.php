5a6997789d9d9e12c4f7d5e751c7c05be3fb5a8e
bcel15 <betulcelikoz77@gmail.com>
2025-01-07 21:34:18 +0100

    modif accueil

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <!-- Menu de navigation -->
 <nav>
     <ul>
-        <li><a href="accueil.php">Accueil</a></li>
+        <li><a href="index.php">Accueil</a></li>
         <li><a href="contact.php">Contacter</a></li>
         <li><a href="catalogue.php">Catalogue</a></li>
         <li><a href="panier.php">Panier</a></li>
