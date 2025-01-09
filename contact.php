04a448252d14e7f8766fc20084637c96f826bc30
OceaneDks <oceaane.dickson@gmail.com>
2025-01-09 09:09:01 +0100

    caca

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@
 <body>
     <header>
         <div class="header-banner">
-            <img src="img/baniere_cp.jpg" alt="Bannière Magasin de Parfums">
+            <img src="img/baniere_cp" alt="Bannière Magasin de Parfums">
             <h1>Magasin de Parfums</h1>
         </div>
     </header>
