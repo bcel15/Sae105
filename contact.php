783185279b8433e185135b87e8e544b4a37d68fd
OceaneDks <oceaane.dickson@gmail.com>
2025-01-09 08:49:59 +0100

    caca

--- contact.php
+++ contact.php
@@ -15,9 +15,6 @@
                 <img src="img/logo.png" alt="Logo Magasin de Parfums">
             </div>
             <h1>Magasin de Parfums</h1>
-            <div class="cart-link">
-                <a href="cart.php">Panier</a>
-            </div>
         </div>
     </header>
 
@@ -26,6 +23,7 @@
             <li><a href="index.php">Accueil</a></li>
             <li><a href="catalog.php">Produits</a></li>
             <li><a href="contact.php">Contact</a></li>
+            <li><a href="cart.php" class="cart-link">Panier</a></li>
         </ul>
     </nav>
 
@@ -37,37 +35,37 @@
         </section>
 
         <section class="contact-form">
-    <h3>Contactez-nous</h3>
-    <form action="submit_contact.php" method="post">
-        <div class="form-group">
-            <label for="prenom">Prénom <span>*</span></label>
-            <input type="text" id="prenom" name="prenom" required>
-        </div>
-        <div class="form-group">
-            <label for="nom">Nom <span>*</span></label>
-            <input type="text" id="nom" name="nom" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Email <span>*</span></label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="adresse">Adresse postale</label>
-            <textarea id="adresse" name="adresse" rows="2"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="telephone">Téléphone</label>
-            <input type="tel" id="telephone" name="telephone">
-        </div>
-        <div class="form-group">
-            <label for="message">Votre message</label>
-            <textarea id="message" name="message" rows="4"></textarea>
-        </div>
-        <div class="form-group">
-            <button type="submit">Envoyer</button>
-        </div>
-    </form>
-</section>
+            <h3>Contactez-nous</h3>
+            <form action="submit_contact.php" method="post">
+                <div class="form-group">
+                    <label for="prenom">Prénom <span>*</span></label>
+                    <input type="text" id="prenom" name="prenom" required>
+                </div>
+                <div class="form-group">
+                    <label for="nom">Nom <span>*</span></label>
+                    <input type="text" id="nom" name="nom" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email <span>*</span></label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="adresse">Adresse postale</label>
+                    <textarea id="adresse" name="adresse" rows="2"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="telephone">Téléphone</label>
+                    <input type="tel" id="telephone" name="telephone">
+                </div>
+                <div class="form-group">
+                    <label for="message">Votre message</label>
+                    <textarea id="message" name="message" rows="4"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit">Envoyer</button>
+                </div>
+            </form>
+        </section>
 
         <section class="contact-details">
             <div class="contact-left">
@@ -100,6 +98,4 @@
         </div>
     </footer>
 </body>
-
-
 </html>
