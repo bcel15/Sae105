3350fed022cd7d783f4dd1182e255f40df713926
OceaneDks <oceaane.dickson@gmail.com>
2025-01-15 16:40:30 +0100

    modif panier

--- panier.php
+++ panier.php
@@ -38,7 +38,7 @@
 
         <button id="validerPanier">Valider le panier</button>
     </main>
-    <script src="panier.js" defer></script>
-    <script src="catalogue.js" defer></script>
+
+    <script src="panier.js"></script>
 </body>
 </html>
