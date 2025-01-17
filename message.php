c49530bf2da028693f8f6757de200ad6d1f490d1
OceaneDks <oceaane.dickson@gmail.com>
2025-01-17 18:01:16 +0100

    modif php

--- message.php
+++ message.php
@@ -11,7 +11,7 @@
         <section class="hero-contact">
             <h2>Votre message a bien été pris en compte</h2>
             <p>Nous vous remercions pour votre prise de contact. Notre équipe vous répondra dans les meilleurs délais.</p>
-            <a href="index.html" class="button">Retour à l'accueil</a>
+            <a href="index.php" class="button">Retour à l'accueil</a>
         </section>
     </main>
 </body>
