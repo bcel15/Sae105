a4c11e11445fbb54a3dca4ad68017085d2fabc36
OceaneDks <oceaane.dickson@gmail.com>
2025-01-28 21:35:55 +0100

    modif java carousselle

--- homepage.js
+++ homepage.js
@@ -1,18 +1,13 @@
 // Carrousel défilant
 const carrouselImages = document.querySelector('.carrousel-images');
+let index = 0;
 
-if (carrouselImages) {  
-    let index = 0;
-    const totalImages = carrouselImages.children.length; // Récupère dynamiquement le nombre d'images
-
-    function defilerCarrousel() {
-        index = (index + 1) % totalImages;
-        const offset = -index * 100; // Décalage en pourcentage
-        carrouselImages.style.transform = `translateX(${offset}%)`;
-    }
-
-    // Défilement automatique toutes les 3 secondes
-    setInterval(defilerCarrousel, 3000);
-} else {
-    console.warn('L\'élément .carrousel-images est introuvable.');
+function defilerCarrousel() {
+    index = (index + 1) % 3; // 6 images au total
+    const offset = -index * 100; // Décalage en pourcentage
+    carrouselImages.style.transform = `translateX(${offset}%)`;
 }
+
+// Défilement automatique toutes les 3 secondes
+setInterval(defilerCarrousel, 3000);
+
